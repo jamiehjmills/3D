850c216067d24e29e4ff39c8744546dc5b901e03
Hyojoo Mills <hyojoo.mills@gmail.com>
2025-03-01 20:15:20 +1100

    created planets

---
+++ planet.py
@@ -0,0 +1,12 @@
+from ursina import *
+
+class Planet():
+    def __init__(self):
+        self.entities = {}
+
+    def create_entity(self, name, radius, distance, color):
+         entity = Entity(model='sphere', color=color, scale=(radius, radius, radius), position=(distance, 0, 0))
+         self.entities[name] = entity
+         return entity
+
+
--- solarSystem.py
+++ solarSystem.py
@@ -1,32 +1,95 @@
 from ursina import *
+from planet import Planet
+
 
 app = Ursina()
+solar_system = Planet()
 
-orbit_radius=3
+# Set the window size
+window.size = (3000, 1280)  # Width, Height
+window.title = 'Solar System Simulation'  # Set the window title
+
+#orbit_radius=1
 x=0
 speed=1
-angle=0
+angle=3
+angle_mercury = 1
+angle_venus = 1
+angle_earth = 1
+angle_mars = 1
+angle_jupiter = 1
+angle_saturn = 1
+angle_uranus = 1
+angle_neptune = 1
 
 # Create a sphere entity
-sun = Entity(model='sphere', color=color.yellow, scale=(1, 1, 1), position=(0, 0, 0))
-earth = Entity(model='sphere', color=color.green, scale=0.5, position=(3, 0, 0))
+#sun = Entity(model='sphere', color=color.yellow, scale=1, position=(0, 0, 0))
+#mercury = Entity(model='sphere', color=color.green, scale=0.04, position=(0, 0, 0))
+sun = solar_system.create_entity('sun', 1, 0, color.yellow)
+mercury = solar_system.create_entity('mercury', 0.04, 0, '#B7B8B9')
+venus = solar_system.create_entity('venus', 0.09, 0, '#877882')
+earth = solar_system.create_entity('earth', 0.09, 0, '#6b93d6')
+mars = solar_system.create_entity('mars', 0.05, 0, '#c1440e')
+jupiter = solar_system.create_entity('jupiter', 1.03, 0, '#c99039')
+saturn = solar_system.create_entity('saturn', 0.87, 0, '#e3e0c0')
+uranus = solar_system.create_entity('uranus', 0.37, 0, '#ACE5EE')
+neptune = solar_system.create_entity('neptune', 0.36, 0, '#5b5ddf')
 
 def update():
     # global x, speed
     # x += time.dt*speed
-    global angle
-    angle += time.dt * speed
+    global angle_mercury, angle_venus, angle_earth, angle_mars, angle_jupiter, angle_saturn, angle_uranus, angle_neptune
+    #angle += time.dt * speed
+
+    sun.rotation_y += 1  # Rotate the sphere around the y-axis
+
+    # mercury
+    angle_mercury += time.dt * speed
+    mercury.x = 1.158 * math.cos(angle_mercury)
+    mercury.y = 1.158 * math.sin(angle_mercury)
+
+    # venus
+    angle_venus += time.dt * speed
+    venus.x = 2.164 * math.cos(angle_venus)
+    venus.y = 2.164 * math.sin(angle_venus)
+
+    # earth
+    angle_earth += time.dt * speed
+    earth.x = 2.992 * math.cos(angle_earth)
+    earth.y = 2.992 * math.sin(angle_earth)
+
+    # mars
+    angle_mars += time.dt * speed
+    mars.x = 4.558 * math.cos(angle_mars)
+    mars.y = 4.558 * math.sin(angle_mars)
+
+    # jupiter
+    angle_jupiter += time.dt * speed
+    jupiter.x = 5.572 * math.cos(angle_jupiter)
+    jupiter.y = 5.572 * math.sin(angle_jupiter)
+
+    # saturn
+    angle_saturn += time.dt * speed
+    saturn.x = 7.67 * math.cos(angle_saturn)
+    saturn.y = 7.67 * math.sin(angle_saturn)
+
+    # uranus
+    angle_uranus += time.dt * speed
+    uranus.x = 8.45 * math.cos(angle_uranus)
+    uranus.y = 8.45 * math.sin(angle_uranus)
+
+    # neptune
+    angle_neptune += time.dt * speed
+    neptune.x = 9.902 * math.cos(angle_neptune)
+    neptune.y = 9.902 * math.sin(angle_neptune)
+
+    camera.position = (0, 0, 1)  # Example position
+
 
     # if abs(x) > 3:
     #     speed *= -1
 
-    # camera.position=(0, 0, -20)
-
-    sun.rotation_y += 1  # Rotate the sphere around the y-axis
-    earth.x = orbit_radius * math.cos(angle)
-    #earth.y = orbit_radius * math.sin(angle)
-    earth.z = orbit_radius * math.sin(angle)
 
 app.run()
 
-## TO DO: create the rest of the planets in the solar system!! (28 Feb 2024)
+## TO DO: https://www.ursinaengine.org/entity_basics.html#Position <-- read documentation!!
